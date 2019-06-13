34d302ec08ef70118637123cb24958456acce59b
Phaysik <mmoore0111@gmail.com>
2019-06-12 23:48:35 -0500

    User Signup Validation Annotations

--- Unit 3/cheese-mvc/src/main/java/cheesemvc/controllers/UserController.java
+++ Unit 3/cheese-mvc/src/main/java/cheesemvc/controllers/UserController.java
@@ -3,9 +3,12 @@ package cheesemvc.controllers;
 import cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -19,21 +22,20 @@ public class UserController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
         model.addAttribute("title", "Create an Account");
+        model.addAttribute(new User());
 
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
-        if (user.getPassword().equals(verify)) {
-            return "redirect:";
-        } else {
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if (errors.hasErrors()) {
             model.addAttribute("title", "Create an Account");
-            model.addAttribute("passwordError", "Error");
-            model.addAttribute("uName", user.getUsername());
-            model.addAttribute("uMail", user.getEmail());
+            model.addAttribute(user);
 
             return "user/add";
+        } else {
+            return "redirect:";
         }
     }
 }
--- Unit 3/cheese-mvc/src/main/java/cheesemvc/models/User.java
+++ Unit 3/cheese-mvc/src/main/java/cheesemvc/models/User.java
@@ -1,9 +1,25 @@
 package cheesemvc.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotNull
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 6)
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User(String username, String email, String password) {
         this.username = username;
@@ -39,5 +55,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (!this.getPassword().equals(this.getVerifyPassword()) && this.getPassword() != null && this.getVerifyPassword() != null) {
+            this.verifyPassword = null;
+        }
     }
 }
